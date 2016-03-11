ad0e7390985545c56f35ae49de9b49dec4e627eb
Jackson Collins <jc5wn@virginia.edu>
2016-03-11 13:57:43 -0500

    Made all but name and email fields not required

--- mysite/forms.py
+++ mysite/forms.py
@@ -6,13 +6,13 @@ class ContactForm(forms.Form):
     fname = forms.CharField(label='First Name', max_length=100)
     lname = forms.CharField(label='Last Name', max_length=100)
     email = forms.EmailField()
-    org = forms.CharField(label='Organization', max_length=100)
-    title = forms.CharField(label='Title', max_length=100)
+    org = forms.CharField(label='Organization', max_length=100, required=False)
+    title = forms.CharField(label='Title', max_length=100, required=False)
     checkbox = []
     for i in range(9):
         checkbox.append(i)
         checkbox[i] = forms.BooleanField(required=False)
-    message = forms.CharField(label='Message', widget=forms.Textarea)
+    message = forms.CharField(label='Message', widget=forms.Textarea, required=False)
 
     # proof_of_publication = forms.ChoiceField(
     #     label="Proof of publication",
