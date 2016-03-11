7267ca73574344cdf6e555b8c79503d44e12a9c9
Jackson Collins <jc5wn@virginia.edu>
2016-03-11 15:23:37 -0500

    remove irrelevant comments

--- mysite/forms.py
+++ mysite/forms.py
@@ -13,33 +13,3 @@ class ContactForm(forms.Form):
         checkbox.append(i)
         checkbox[i] = forms.BooleanField(required=False)
     message = forms.CharField(label='Message', widget=forms.Textarea, required=False)
-
-    # proof_of_publication = forms.ChoiceField(
-    #     label="Proof of publication",
-    #     choices=(
-    #         ("not_submitted", "Published Article Not Yet Submitted"),
-    #         ("submitted", "Published Article Submitted"),
-    #         ("under_review", "Published Article Under Review"),
-    #         ("approved", "Published Article Approved"),
-    #         ("rejected", "Published Article Rejected"),
-    #     )
-    # )
-    # payment_sent = forms.BooleanField(
-    #     label="Payment sent",
-    #     required=False
-    # )
-    # assignee = forms.ChoiceField(
-    #     label="Assignee",
-    #     choices=list(),
-    #     required=False,
-    # )
-    #
-    # notes = forms.CharField(
-    #     label="Notes",
-    #     widget=forms.Textarea(
-    #         attrs={
-    #             'class': 'prereg-form-notes'
-    #         }
-    #     ),
-    #     required=False
-    # )
